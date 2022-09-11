93952811ef0626a94c3338044b623181a4e8f67a
Hizari <hhizari@gmail.com>
2022-09-11 19:25:39 +0700

    Initial commit

---
+++ app/src/main/java/id/hizari/soundtweet/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.hizari.soundtweet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
